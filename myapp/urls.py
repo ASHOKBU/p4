1d08e0ebd1961b42a75bfe52b4b0f737c70b1757
unknown <ashokbuengg@gmail.com>
2020-07-27 18:31:37 +0530

    fourth commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ app_name = "myapp"
 urlpatterns=[
     path('',views.index,name="index"),
     path('home/', views.home, name="home"),
-    path('fact/<n>/', views.fact, name="fact")
+    path('fact/<n>/', views.fact, name="fact"),
+    path('child/', views.child,name="child")
 ]
